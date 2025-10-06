da952843e81ac479605dc7df3de0e45d80b640d2
Hem Gajjar <hem.gajjar@bypeopletechnologies.com>
2025-10-06 12:43:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-uvicorn app:app --reload
+#!/bin/bash
+# Ensure correct Python version
+echo "Using Python: $(python --version)"
+# Install dependencies
+pip install -r requirements.txt
+# Start FastAPI app
+uvicorn app:app --host 0.0.0.0 --port $PORT --reload
